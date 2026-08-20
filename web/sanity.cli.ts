f73eb3b37eb4e6217b662c34b893830c8ad25fc5
Mitchell Christ <mitchell@human.marketing>
2026-08-19 21:14:17 -0700

    update projectId

--- web/sanity.cli.ts
+++ web/sanity.cli.ts
@@ -6,7 +6,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
 	api: {
-		projectId: 'cyu7k2r0',
+		projectId: 'zd7g2ch2',
 		dataset: 'production',
 	},
 	typegen: {
